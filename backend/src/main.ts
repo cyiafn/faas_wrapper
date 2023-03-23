d1b80709c8a869023d6783c3b47d746024927d03
cyiafn <dev@cyifan.dev>
2023-03-23 17:37:27 +0800

    feat: enable cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: true, // Allow all origins
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: '*',
+    credentials: true,
+  });
   await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
